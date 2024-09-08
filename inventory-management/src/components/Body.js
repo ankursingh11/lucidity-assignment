f67645362a7380356e8cfccd930ccec9fbce2d0f
Ankur Singh <70473170+ankursingh11@users.noreply.github.com>
2024-09-08 17:20:04 +0530

    Edit button modal open

--- inventory-management/src/components/Body.js
+++ inventory-management/src/components/Body.js
@@ -3,34 +3,65 @@ import useInventoryData from "../utils/useInventoryData";
 import { DUMMY_DATA } from "../constants/dummyData";
 import KPI from "./KPISection";
 import InventoryTable from "./Table";
-import { findTotalProduts, findTotalValue, findOutOfStock, findCategory } from "../utils/utilFunctions";
-
+import {
+  findTotalProduts,
+  findTotalValue,
+  findOutOfStock,
+  findCategory,
+} from "../utils/utilFunctions";
+import EditModal from "./EditModal";
 
 const Body = () => {
   // const data = useInventoryData();
   const [inventoryData, setInventoryData] = useState(DUMMY_DATA);
-  const [totalProducts, setTotalProducts] = useState(() => findTotalProduts(inventoryData));
-  const [totalValue, setTotalValue] = useState(() => findTotalValue(inventoryData));
-  const [outOfStock, setOutOfStock] = useState(() => findOutOfStock(inventoryData));
+  const [totalProducts, setTotalProducts] = useState(() =>
+    findTotalProduts(inventoryData)
+  );
+  const [totalValue, setTotalValue] = useState(() =>
+    findTotalValue(inventoryData)
+  );
+  const [outOfStock, setOutOfStock] = useState(() =>
+    findOutOfStock(inventoryData)
+  );
   const [category, setCategory] = useState(() => findCategory(inventoryData));
+  const [showModal, setShowModal] = useState(false);
+  const [x, setX] = useState(0);
 
   return (
-    <div className="m-6">
-      <div className="text-4xl my-7">Inventory stats</div>
-      <KPI
-        totalProducts={totalProducts}
-        totalValue={totalValue}
-        outOfStock={outOfStock}
-        category={category}
-      />
-      <InventoryTable
-        inventoryData={inventoryData}
-        setInventoryData={setInventoryData}
-        setTotalProducts={setTotalProducts}
-        setTotalValue = {setTotalValue}
-        setOutOfStock = {setOutOfStock}
-        setCategory = {setCategory}
-      />
+    <div>
+      {showModal && (
+        <EditModal
+          x={x}
+          setShowModal={setShowModal}
+          inventoryData={inventoryData}
+          setInventoryData={setInventoryData}
+          setTotalProducts={setTotalProducts}
+          setTotalValue={setTotalValue}
+          setOutOfStock={setOutOfStock}
+          setCategory={setCategory}
+        />
+      )}
+
+      <div className={"m-6" + (showModal ? " opacity-50" : "")}>
+        <div className="text-4xl my-7">Inventory stats</div>
+        <KPI
+          totalProducts={totalProducts}
+          totalValue={totalValue}
+          outOfStock={outOfStock}
+          category={category}
+        />
+        <InventoryTable
+          inventoryData={inventoryData}
+          setInventoryData={setInventoryData}
+          setTotalProducts={setTotalProducts}
+          setTotalValue={setTotalValue}
+          setOutOfStock={setOutOfStock}
+          setCategory={setCategory}
+          showModal={showModal}
+          setShowModal={setShowModal}
+          setX={setX}
+        />
+      </div>
     </div>
   );
 };
--- inventory-management/src/components/Table.js
+++ inventory-management/src/components/Table.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { TABLE_HEADINGS } from "../constants/constants";
-import {
-  faPen,
-  faEye,
-  faTrash,
-  faEyeSlash,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPen, faEye, faTrash, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSelector } from "react-redux";
-import { findTotalProduts, findTotalValue, findOutOfStock, findCategory } from "../utils/utilFunctions";
+import {
+  findTotalProduts,
+  findTotalValue,
+  findOutOfStock,
+  findCategory,
+} from "../utils/utilFunctions";
 
 const InventoryTable = ({
   inventoryData,
@@ -16,10 +16,12 @@ const InventoryTable = ({
   setTotalProducts,
   setTotalValue,
   setOutOfStock,
-  setCategory
+  setCategory,
+  setShowModal,
+  showModal,
+  setX
 }) => {
   const isAdmin = useSelector((store) => store.user.isAdmin);
-
   function handleDisable(index) {
     inventoryData[index].disable = !inventoryData[index].disable;
     setInventoryData([...inventoryData]);
@@ -29,13 +31,18 @@ const InventoryTable = ({
     setCategory(() => findCategory(inventoryData));
   }
 
-  function handleDelete(index){
+  function handleDelete(index) {
     inventoryData.splice(index, 1);
     setInventoryData([...inventoryData]);
     setTotalProducts(() => findTotalProduts(inventoryData));
     setTotalValue(() => findTotalValue(inventoryData));
     setOutOfStock(() => findOutOfStock(inventoryData));
     setCategory(() => findCategory(inventoryData));
+  }
+
+  function handleEditModal(index) {
+    setX(index);
+    setShowModal(!showModal);
   }
 
   return (
@@ -63,14 +70,15 @@ const InventoryTable = ({
                   <td>{data?.quantity}</td>
                   <td>{data?.value}</td>
                   <td>
-                    <button disabled={isAdmin === false}>
+                    <button
+                      disabled={isAdmin === false}
+                      onClick = {() => handleEditModal(index)}
+                    >
                       <FontAwesomeIcon
                         icon={faPen}
                         className={
                           "mx-2" +
-                          (isAdmin && !data?.disable
-                            ? " text-green-700 cursor-pointer"
-                            : "")
+                          (isAdmin ? " text-green-700 cursor-pointer" : "")
                         }
                       />
                     </button>
@@ -86,7 +94,10 @@ const InventoryTable = ({
                         }
                       />
                     </button>
-                    <button disabled={isAdmin === false} onClick = {() => handleDelete(index)}>
+                    <button
+                      disabled={isAdmin === false}
+                      onClick={() => handleDelete(index)}
+                    >
                       <FontAwesomeIcon
                         icon={faTrash}
                         className={
