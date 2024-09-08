3a530d0d82f9a13631caddcd41bb45f70329e65e
Ankur Singh <70473170+ankursingh11@users.noreply.github.com>
2024-09-08 11:02:34 +0530

    Attached a dispatch action to toggle admin/user

--- inventory-management/src/components/Navbar.js
+++ inventory-management/src/components/Navbar.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch } from "react-redux";
+import { toggleAdmin } from "../utils/userSlice";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  function handleToggle() {
+    dispatch(toggleAdmin());
+  }
+
   return (
     <div>
       <div className="navbar">
         <div className="navbar-start">
-          <span className="navbar-item font-bold text-xl first-letter">Inventory Management</span>
+          <span className="navbar-item font-bold text-xl first-letter">
+            Inventory Management
+          </span>
         </div>
         <div className="navbar-end mx-6">
           <span>admin</span>
           <input
             type="checkbox"
             className="switch mx-3"
-            onClick={() => console.log("clicked")}
+            onClick={handleToggle}
           />
           <span>user</span>
         </div>
-        <FontAwesomeIcon icon={faRightFromBracket} className = "cursor-pointer"/>
+        <FontAwesomeIcon icon={faRightFromBracket} className="cursor-pointer" />
       </div>
     </div>
   );
--- inventory-management/src/utils/userSlice.js
+++ inventory-management/src/utils/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    isAdmin: false,
+    isAdmin: true,
   },
   reducers: {
     toggleAdmin: (state, action) => {
