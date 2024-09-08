41acd4b8ad2c81a8cafc1a8a1ec8d9ca9c76d164
Ankur Singh <70473170+ankursingh11@users.noreply.github.com>
2024-09-08 10:54:55 +0530

    created a userSlice

---
+++ inventory-management/src/utils/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    isAdmin: false,
+  },
+  reducers: {
+    toggleAdmin: (state, action) => {
+      state.isAdmin = !state.isAdmin;
+    },
+  },
+});
+
+export const { toggleAdmin } = userSlice.actions;
+export default userSlice.reducer;
